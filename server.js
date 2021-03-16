6f3d230c4121559a179e0da54396e229820a56e9
Rachel Thompson <thompson.1657@gmail.com>
2021-03-15 20:37:37 -0500

    deploying

--- server.js
+++ server.js
@@ -14,8 +14,12 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(
-    process.env.MONGODB_URI || 'mongodb://localhost/workout',
-    {useNewUrlParser: true, useUnifiedTopology: true}
+    process.env.MONGODB_URI || 'mongodb://localhost/workout', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    }
   );
 
 require("./routes/apiRoutes.js")(app)
